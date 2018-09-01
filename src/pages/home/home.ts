15285e3e233181c01ae99a22112645e7ce1d3eb5
Kayo <kayo@tosotolabs.info>
2018-08-31 22:46:57 -0400

    aesthetics

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { MenuController } from 'ionic-angular';
 
 @Component({
@@ -18,63 +18,4 @@ export class HomePage {
         });
     }
 
-}
-
-let setupTargetSignal = (canvas,hsc,vsc)=> {
-    // let ctx = canvas.getContext('2d');
-    // let tgChart = new Chart(ctx, {
-    //     type: 'line',
-    //     data: {
-    //         labels: ["0.60", "0.97", "0.15", "0.25", "0.10", "0.15"],
-    //         datasets: [{
-    //             label: '',
-    //             data: [0.6, 0.97, 0.15, 0.25, 0.1, 0.15],
-    //             backgroundColor: 'rgba(235,235,235,0.2)',
-    //             borderColor: 'rgba(100,100,100,0.4)',
-    //             borderWidth: 1,
-    //             lineTension: 0,
-    //             pointBackgroundColor: ['red','blue','yellow','green','purple','orange'],
-    //             pointBorderColor: 'rgba(0,0,0,0)',
-    //             pointHitRadius: 15
-    //
-    //         }]
-    //     },
-    //     options: {
-    //         responsive: false,
-    //         scales: {
-    //             xAxes: [{
-    //                 ticks: {
-    //                     autoSkip: false,
-    //                     fontSize: 9,
-    //                     fontFamily: "'Times New Roman', serif"
-    //                 },
-    //                 gridLines: {
-    //                     display: true
-    //                 }
-    //             }],
-    //             yAxes: [{
-    //                 ticks: {
-    //                     min: 0,
-    //                     max: 1,
-    //                     fontSize: 9,
-    //                     fontFamily: "'Times New Roman', serif"
-    //                 },
-    //                 gridLines: {
-    //                     display: true
-    //                 },
-    //                 scaleLabel: {
-    //                     fontSize: 9,
-    //                     fontFamily: "'Times New Roman', serif"
-    //                 }
-    //             }]
-    //         },
-    //         legend: {
-    //             display: false
-    //         },
-    //         tooltips: {
-    //             enabled: false
-    //         }
-    //
-    //     }
-    // });
 }
