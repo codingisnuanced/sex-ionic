b664ff7c5a98cfd1542d376754c22b0e1cdc52d8
Kayo <kayo@tosotolabs.info>
2018-09-02 21:54:32 -0400

    the piece

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,11 @@ import { MenuController } from 'ionic-angular';
 })
 export class HomePage {
 
+    thepiece: Object;
+
     constructor(public menu: MenuController) {
         menu.enable(true);
+        this.thepiece = new Piece('black',2);
     }
 
     ngAfterViewInit() {
@@ -16,6 +19,19 @@ export class HomePage {
         window['particlesJS'].load('page-home-content', 'assets/config/particles.json', function() {
             console.log('callback - particles.js config loaded');
         });
+        console.log(this.thepiece);
     }
 
+}
+
+let Piece = function(side,colorIndex) {
+    this.side = side;
+    this.colorIndex = colorIndex;
+}
+
+let BoardSquare = function(piece,selectedBy,component,isDisturbed) {
+    this.piece = piece;
+    this.selectedBy = selectedBy;
+    this.component = component;
+    this.isDisturbed = isDisturbed;
 }
